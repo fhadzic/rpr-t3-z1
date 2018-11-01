cbbca337d6503f4666d3290d008b3035f406e14c
fhadzic <fhadzic1@etf.unsa.ba>
2018-11-01 10:30:31 +0100

    Dodao nove metode u klasi Imenik

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,13 +1,40 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Imenik {
 
     private Map<String, TelefonskiBroj> hashPoImenu = new HashMap<>();
-    private Map<TelefonskiBroj, String> hashPoTelefonskomBroju = new HashMap<>();
 
     public void dodaj(String ime, TelefonskiBroj broj) {
         hashPoImenu.put(ime, broj);
-        hashPoTelefonskomBroju.put(broj, ime);
     }
+
+    public String dajBroj(String ime){
+        return hashPoImenu.get(ime).ispisi();
+    }
+
+    public String dajIme(TelefonskiBroj broj){
+        String ime;
+        for( Map.Entry<String, TelefonskiBroj> el : hashPoImenu.entrySet() ){
+            if(el.getValue() == broj ){
+                ime = el.getKey();
+                break;
+            }
+        }
+        return ime;
+    }
+
+    public String naSlovo(char s){
+        for( Map.Entry<String, TelefonskiBroj> el : hashPoImenu.entrySet() ){
+            String key = el.getKey();
+            if(key.charAt(0) == s){
+                 = key;
+                break;
+            }
+        }
+    }
+
 }
